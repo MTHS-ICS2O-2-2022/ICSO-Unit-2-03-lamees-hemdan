a71c78c1d0c85ea4ec19ddb9132d8b887a39474b
lamees-hemdan <124389652+lamees-hemdan@users.noreply.github.com>
2023-03-20 17:12:42 +0000

    added html and js

--- js/script.js
+++ js/script.js
@@ -3,3 +3,16 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+/**
+ * This function gets users first name and age and shows it back to user.
+ */
+function enterClicked() {
+  // input
+  const firstName = document.getElementById("Street-Number").value
+  const userAge = parseInt(document.getElementById("Street-Number").value)
+
+  // output
+  document.getElementById("Street-Name").innerHTML =
+    "Your info is: " + Street-Number + ", Street-Name " + userAge + "."
+}
